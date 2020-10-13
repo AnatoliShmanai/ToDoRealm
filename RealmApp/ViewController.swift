a2e5d2d6e23b8727e48b8b83881f8a75c9254878
Anatoli Shmanai <vintagemixing@gmail.com>
2020-10-13 08:27:56 +0300

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by shmanai on 10/13/20.
+//  Copyright © 2020 Anatoli Shmanai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
